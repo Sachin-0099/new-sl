6ed77b76387b4f24f04dacbc8a3975c691182994
Dwivedi Priya <pd7668665@gmail.com>
2025-03-17 19:13:31 +0530

    layout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,16 +3,15 @@ import about2 from '/images/about2.png'
 const About = () => {
   return (
     <>
-    <div className='min-h-full w-screen flex flex-wrap lg:flex-col md:flex-col sm:flex-row justify-center items-center'>
-        <div className='h-[95%] w-[90%] m-10 flex flex-wrap'>
-            <div className='lg:h-full lg:w-[90vh] md:h-full md:w-[50vh] w-full h-[40vh]'>
+    <div className='min-h-[90vh] w-screen flex flex-wrap mt-5 lg:flex-col md:flex-col sm:flex-row justify-center items-center'>
+        <div className='h-[70vh] w-[100%] flex flex-wrap'>
+            <div className='lg:h-full lg:w-[90vh] md:h-[70%] md:w-[50vh] w-full h-[40vh]'>
                 <img src={about2} className='w-full h-full object-cover'/>
             </div>
-            <div className='lg:h-full lg:w-[50%] md:h-full md:w-[50%] w-full h-[50vh]'>
+            <div className='lg:h-full lg:w-[50%] md:h-full md:w-[55%] w-full h-[50vh]'>
                 <div className='flex flex-wrap items-center justify-center'>
                     <h1 className='lg:text-6xl md:text-3xl mb-2 text-3xl font-semibold text-[#3087D1] underline underline-offset-8'>About Us</h1>
-                    <p className='lg:text-xl md:text-sm text-gray-700 text-xs p-2 md:p-3 lg:p-8 leading-4 md:leading-4 lg:leading-10'>My journey has taken me across the United Arab Emirates, Thailand, parts of Africa, and Pakistan, enriching my creative vision and expertise. Whether it's dynamic social media content, fashion and modeling shoots, immersive 360° real estate coverage, construction timelapses, stunning drone visuals, intimate portraits, food restaurant imagery, or lively corporate events, weddings, birthdays, and product shoots, I bring passion and precision to every project.</p>
-                    <p className='lg:text-xl md:text-sm text-gray-700 text-xs p-2 md:p-3 lg:p-8 leading-4 md:leading-4 lg:leading-10'>Now, I'm excited to elevate my creative horizons further as I venture into Saudi Arabia through a vibrant partnership with “Slasa”, promising fresh perspectives and innovative storytelling in every frame.</p>
+                    <p className='lg:text-xl md:text-md text-gray-700 text-xs p-2 md:p-3 lg:p-8 leading-4 md:leading-6 lg:leading-10'>My journey has taken me across the United Arab Emirates, Thailand, parts of Africa, and Pakistan, enriching my creative vision and expertise. Whether it's dynamic social media content, fashion and modeling shoots, immersive 360° real estate coverage, construction timelapses, stunning drone visuals, intimate portraits, food restaurant imagery, or lively corporate events, weddings, birthdays, and product shoots, I bring passion and precision to every project.<br/> Now, I'm excited to elevate my creative horizons further as I venture into Saudi Arabia through a vibrant partnership with “Slasa”, promising fresh perspectives and innovative storytelling in every frame.</p>
                 </div>
             </div>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,15 +5,15 @@ import logo from '/images/logo.png'
 const Header = () => {
   return (
     <>
-    <div className='h-[90vh] w-screen flex flex-wrap lg:flex-col md:flex-col sm:flex-row justify-center items-center'>
-        <div className='h-[90%] w-[90%] m-10 flex flex-wrap'>
-            <div className='lg:h-full lg:w-[60%] md:h-full md:w-[60%] w-full h-[40vh]'>
+    <div className='min-h-[60vh] w-screen flex flex-wrap lg:flex-col md:flex-col sm:flex-row justify-start items-center'>
+        <div className='h-[60vh] w-[100%] flex flex-wrap'>
+            <div className='lg:h-full lg:w-[50%] md:h-full md:w-[50%] w-full h-[20vh]'>
                 <div className='flex flex-wrap items-center justify-center'>
-                    <img src={logo} alt="" className='lg:w-[90vh] lg:h-[36vh] md:w-[50vh] md:h-[20vh] h-[10vh] w-[30vh] mt-10' />
-                    <p className='lg:text-2xl md:text-xl text-sm p-4 md:p-10 lg:p-10 font-semibold leading-5 md:leading-7 lg:leading-10'>Hi, This is Emad Captures—an accomplished photographer and videographer with over a decade of experience capturing the refined details and unique narratives that define each scene.</p>
+                    <img src={logo} alt="" className='lg:w-[90vh] lg:h-[36vh] md:w-[50%] md:h-[20vh] h-[15vh] w-[30vh] object-cover' />
+                    <p className='lg:text-6xl md:text-3xl mt-2 font-semibold text-3xl text-transparent bg-gradient-to-r from-[#3087D1] via-[#3087D5] to-black bg-clip-text'>Emad Captures</p>
                 </div>
             </div>
-            <div className='lg:h-full lg:w-[72vh] md:h-full md:w-[40vh] w-full h-[40vh]'>
+            <div className='lg:h-full bg-blue-500 lg:w-[50%] md:h-full md:w-[50%] w-full h-[35vh]'>
                 <img src={about} className='w-full h-full object-cover'/>
             </div>
         </div>
